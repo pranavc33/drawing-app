6b89e1539e4501f8c3c81452754283190cd635d4
Pranav Chavare <pranavchavare33@gmail.com>
2023-02-03 00:02:19 +0530

    adding custompath

--- app/src/main/java/io/example/drawingapp/DrawingView.kt
+++ app/src/main/java/io/example/drawingapp/DrawingView.kt
@@ -16,6 +16,8 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs) {
     private var mBrushSize  : Float = 0.toFloat()
     private var color = Color.BLACK
     private var canvas : Canvas? = null
+    private var mPaths = ArrayList<CustomPath>()
+
 
 
 
@@ -48,11 +50,20 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context,attrs) {
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
     canvas.drawBitmap(mCanvasBitmap!!, 0f,0f,mCanvasPaint)
+        for(path in mPaths){
+            mDrawPaint!!.strokeWidth = path.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(path,mDrawPaint!!)
+
+
+
+        }
 
         if(!mDrawPath!!.isEmpty){
 mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
             mDrawPaint!!.color = mDrawPaint!!.color
             canvas.drawPath(mDrawPath!!,mDrawPaint!!)
+
         }
     }
 
@@ -84,6 +95,7 @@ mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
                 }
             }
             MotionEvent.ACTION_UP -> {
+                mPaths.add(mDrawPath!!)
                 mDrawPath = CustomPath(color, mBrushSize)
             }
             else -> return false
